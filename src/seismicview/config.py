7e9cf457b7fdf81cfec5eb6c81383e46e25d0427
Daniel Ibarrola <dis96051@gmail.com>
2023-08-07 18:17:37 -0600

    Updated config ports

--- src/seismicview/config.py
+++ src/seismicview/config.py
@@ -6,11 +6,11 @@ class Config:
 
     # Address from which the server connects to receive information
     SERVER_HOST_IP = os.environ.get("SERVER_HOST_IP", "localhost")
-    SERVER_HOST_PORT = int(os.environ.get("SERVER_HOST_PORT", 1551))
+    SERVER_HOST_PORT = int(os.environ.get("SERVER_HOST_PORT", 13342))
 
     # Address of the websocket server
     WS_SERVER_HOST_IP = os.environ.get("WS_SERVER_HOST_IP", "localhost")
-    WS_SERVER_HOST_PORT = int(os.environ.get("WS_SERVER_HOST_PORT", 1552))
+    WS_SERVER_HOST_PORT = int(os.environ.get("WS_SERVER_HOST_PORT", 13345))
 
 
 class ProdConfig(Config):
