28b4e21f13fb6bbce9ab45af24307f415cf753f8
Daniel Ibarrola <dis96051@gmail.com>
2024-02-25 09:26:15 -0600

    Ngnix config redirects all paths to index.html

--- init_prod.sh
+++ init_prod.sh
@@ -5,3 +5,8 @@
 export DOMAIN_NAME="http://earthworm.servicios-cires.net/"
 export DB_PASSWORD="Abc123"
 export SECRET_KEY="SuperSecretKey"
+
+export ADMIN_USER=admin@example.com
+export ADMIN_PASSWORD=dog123
+export MAIL_USERNAME=triton@example.com
+export MAIL_PASSWORD=dog
